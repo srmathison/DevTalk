75c1895c3830e4ebeb6f5337c9d9598e118a3e6b
Stephanie <stephanierenee.mathison@gmail.com>
2015-07-07 15:44:06 -0400

    defined post relationships in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+
+  has_many :comments
+  belongs_to :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
+  
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
 end
 
