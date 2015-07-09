139c7b47ebd720c84c59ef61cd3e73e28c34cc07
Stephanie <stephanierenee.mathison@gmail.com>
2015-07-09 13:57:35 -0400

    edited controllers. added 'private' feature. added typing carousel to home page. javascript to application.js

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,31 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.order(id: :desc).limit(5)
+    @posts = Post.order(id: :desc)
   end
 
-  def show
+  def new
+    unless current_user
+      flash[:alert] = "you must log in to create a post"
+      redirect_to posts_path
+    else
+      @post = Post.new
+    end
+  end
+
+    def show
+      @post = Post.find params[:id]
+    end
+    def create
+      @post = current_user.posts.create post_params
+      redirect_to posts_path
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:posttitle, :postbody)
+    end
     
 
 end
