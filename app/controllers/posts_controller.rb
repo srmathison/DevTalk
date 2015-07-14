6ee02b2e16c409549c918155c0f7f8c05dec3752
Stephanie Semerville <stephanie.semerville@gmail.com>
2015-07-14 14:11:51 -0400

    final styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
     def create
       
       @post = current_user.posts.create post_params
-      redirect_to post_path(@post.id)
+      redirect_to posts_path
     end
 
     private
